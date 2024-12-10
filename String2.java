db22a25f1582baf54f9ca82fd3d870ee05dcdce2
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-10 12:06:21 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class String2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+       String a[]={"apple","orange","pineapple"};
+       for(String b:a)
+       System.out.print(b+" ");
+    }
+}
